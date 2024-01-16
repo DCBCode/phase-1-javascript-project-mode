609e38325e920b00395b09b2a8c37855fe0f492f
Drew Benson <Drewbenson16@gmail.com>
2024-01-16 14:32:21 -0700

    added index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  const searchButton = document.getElementById('searchButton')
-  searchButton.addEventListener('click', searchCat)
+  const searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener('click', searchCat);
 });
 
+const catDataUrl = 'http://localhost:3000/data'; // Use the correct URL
 
-const fs = require('fs');
-
-fs.readFile('http://127.0.0.1:3000/data', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const jsonData = JSON.parse(data);
-  console.log(jsonData);
-});
+fetch(catDataUrl)
+  .then(response => response.json())
+  .then(jsonData => {
+    // Use the jsonData to update your webpage
+    console.log(jsonData);
+  })
+  .catch(error => {
+    console.error(error);
+  });
